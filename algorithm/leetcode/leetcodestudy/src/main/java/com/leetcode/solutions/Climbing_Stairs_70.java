1fc59c5950bf6b992f4a21e3d7298d2ee6a8e475
zhangwj0101 <zhangwj0101@gmail.com>
2016-03-01 21:50:49 +0800

    init

--- algorithm/leetcode/leetcodestudy/src/main/java/com/leetcode/solutions/Climbing_Stairs_70.java
+++ algorithm/leetcode/leetcodestudy/src/main/java/com/leetcode/solutions/Climbing_Stairs_70.java
@@ -7,19 +7,26 @@ public class Climbing_Stairs_70 {
 
     public static void main(String[] args) {
         Climbing_Stairs_70 s = new Climbing_Stairs_70();
-        System.out.println(s.climbStairs(3));
+        System.out.println(s.climbStairs(44));
     }
 
-    public int get(int n) {
-        if (n <= 0) {
-            return 0;
+    public int get(int n, int[] table) {
+        if (n <= 1) {
+            return 1;
         }
-    
-        return 2 + get(n - 1) + get(n - 2);
+
+        if (table[n - 1] == 0) {
+            table[n - 1] = get(n - 1, table);
+        }
+        if (table[n - 2] == 0) {
+            table[n - 2] = get(n - 2, table);
+        }
+        return table[n - 1] + table[n - 2];
     }
 
     public int climbStairs(int n) {
+        int[] table = new int[n + 1];
 
-        return get(n);
+        return get(n, table);
     }
 }
