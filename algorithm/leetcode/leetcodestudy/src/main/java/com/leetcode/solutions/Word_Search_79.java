77ae6348dc22b44cf941ce7e9df239cd4542fe27
zhangwj0101 <zhangwj0101@gmail.com>
2016-03-02 12:34:53 +0800

    init

--- algorithm/leetcode/leetcodestudy/src/main/java/com/leetcode/solutions/Word_Search_79.java
+++ algorithm/leetcode/leetcodestudy/src/main/java/com/leetcode/solutions/Word_Search_79.java
@@ -29,8 +29,10 @@ public class Word_Search_79 {
     public boolean exist(char[][] board, String word) {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
-                if (get(board, i, j, 0, word)) {
-                    return true;
+                if (board[i][j] == word.charAt(0)) {
+                    if (get(board, i, j, 0, word)) {
+                        return true;
+                    }
                 }
             }
         }
