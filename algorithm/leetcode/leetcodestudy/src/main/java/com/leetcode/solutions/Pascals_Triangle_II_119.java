22fe147701f78377d5bfb9f5b82fd11e39de2fba
xtayalf <zhangwj0101@gmail.com>
2016-03-12 23:01:39 +0800

    init

--- algorithm/leetcode/leetcodestudy/src/main/java/com/leetcode/solutions/Pascals_Triangle_II_119.java
+++ algorithm/leetcode/leetcodestudy/src/main/java/com/leetcode/solutions/Pascals_Triangle_II_119.java
@@ -1,5 +1,7 @@
 package com.leetcode.solutions;
 
+import org.junit.Test;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -8,32 +10,28 @@ import java.util.List;
  * Created by zwj on 2016/3/12.
  */
 public class Pascals_Triangle_II_119 {
-    public List<List<Integer>> generate(int numRows) {
-
-        List<List<Integer>> results = new ArrayList<>();
-        List<Integer> last = new ArrayList<>();
-        if (numRows < 1) {
-            return results;
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> results = new ArrayList<>();
+        if (rowIndex < 1) {
+            return Arrays.asList(1);
         }
-        results.add(Arrays.asList(1));
-
-        if (numRows == 1) {
-            return results;
-        }
-        last.add(1);
-        last.add(1);
-        results.add(last);
-
-        for (int i = 3; i <= numRows; i++) {
-            List<Integer> tempResults = new ArrayList<>(i);
-            tempResults.add(1);
-            for (int j = 1; j < i - 1; j++) {
-                tempResults.add(last.get(j) + last.get(j - 1));
+        int[] temp = new int[rowIndex + 1];
+        Arrays.fill(temp, 1);
+        for (int i = 1; i < rowIndex; i++) {
+            for (int j = i; j >= 1; j--) {
+                temp[j] += temp[j - 1];
             }
-            tempResults.add(1);
-            results.add(tempResults);
-            last = tempResults;
+        }
+        for (int i = 0; i <= rowIndex; i++) {
+            results.add(temp[i]);
         }
         return results;
     }
+
+    @Test
+    public void get() {
+        System.out.println(getRow(5));
+    }
+
+
 }
