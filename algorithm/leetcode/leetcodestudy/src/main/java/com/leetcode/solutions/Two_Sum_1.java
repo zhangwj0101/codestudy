36e747338b531c0d809ddc88b0ca4626920db2d1
xtayalf <zhangwj0101@gmail.com>
2016-05-05 10:51:28 +0800

    fix

---
+++ algorithm/leetcode/leetcodestudy/src/main/java/com/leetcode/solutions/Two_Sum_1.java
@@ -0,0 +1,20 @@
+package com.leetcode.solutions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by zwj on 2016/5/5.
+ */
+public class Two_Sum_1 {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> maps = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (maps.containsKey(nums[i])) {
+                return new int[]{maps.get(nums[i]), i};
+            }
+            maps.put(target - nums[i], i);
+        }
+        return null;
+    }
+}
